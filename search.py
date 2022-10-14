f19f61a15875bc594fcc019ebccc5b74e9e1140f
Zino Vieth <52859000+ZJVieth@users.noreply.github.com>
2022-10-14 09:42:58 +0900

    Update search.py

--- search.py
+++ search.py
@@ -7,4 +7,10 @@
 
 # Usage:
 # Run passively, one thread will continuously update it's internal database of snippets/topics.
-# Input search terms into 
+# Input search terms into terminal, example:
+# $ find key math
+# => returns list of all math snippets/topics, and links to file names that can be opened in VS Code directly
+# $ find title clamp
+# => returns all snippets with clamp in title
+# $ find desc css animated border
+# => returns all snippets with the words "css", "animated", and "border" in the description
