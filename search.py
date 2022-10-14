dafa48e99f5364633dd80db3c43652817e475bea
Zino Vieth <52859000+ZJVieth@users.noreply.github.com>
2022-10-14 09:33:24 +0900

    Create search.py

---
+++ search.py
@@ -0,0 +1,10 @@
+# Script to scan and search documents for useful snippets and methods.
+
+# Search by:
+# - key (keywords)
+# - title
+# - descr (description)
+
+# Usage:
+# Run passively, one thread will continuously update it's internal database of snippets/topics.
+# Input search terms into 
