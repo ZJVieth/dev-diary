4fee8832a98bef05206959d5a937bbdca4346e9a
Zino Vieth <52859000+ZJVieth@users.noreply.github.com>
2022-10-15 10:50:41 +0900

    Create web-api-spotify.js

---
+++ webdev/web-api-spotify.js
@@ -0,0 +1,7 @@
+/**
+ * # Spotify Web Api
+ * <[api <[js <[spotify <[music
+ *
+ * [https://developer.spotify.com/documentation/web-api/libraries/#web-api-tutorial]
+ * [https://developer.spotify.com/documentation/web-api/]
+ */
