58ba527a2b8d463c6dcb0833aafa72b09abbdba5
Zino Vieth <52859000+ZJVieth@users.noreply.github.com>
2022-10-16 12:33:14 +0900

    Update arrayMethods.js

--- webdev/jsnative/arrayMethods.js
+++ webdev/jsnative/arrayMethods.js
@@ -5,6 +5,8 @@
  * <[list
 
 var n_out = 0
+var bool_result = false
+
 const nr_array = [1, 2, 3, 4, 5]
 const obj_array = [
     {
@@ -37,6 +39,14 @@ n = 0
 n = nr_array.reduce((runningValue, el, i) => runningValue + el)
 // n = 15
 // An initial value can be provided to the reduce method as the second parameter.
+// [https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce]
+
+
+// <[reduceRight
+const n_str = ""
+n_str = nr_array.reduceRight((accumulator, el) => accumulator.concat(el))
+// n_str = "54321"
+// [https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduceRight]
 
 
 // <[filter
@@ -47,6 +57,47 @@ const filtered_nr_array = nr_array.filter(el => el%2 === 0)
 
 // <[find
 // iterates over the array until the first matching element
-const found_obj = obj_array.find(el => el.id === 1002)
+var found_obj = obj_array.find(el => el.id === 1002)
 // { name: "Misses", id: 1002 }
+
+// Alternative find methods
+
+found_obj = obj_array.findLast(el => el.id === 1001)
+// { name: "Mister", id: 1001 }
+
+var found_i = obj_array.findIndex(el => el.id <= 1000)
+// -1 (not found)
+
+found_i = obj_array.findLastIndex(el => el.id >= 1000)
+// 2
+
+// <[every
+const isBelowTen = currValue => currValue < 10
+bool_result = nr_array.every(isBelowTen)
+// true
+// [https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every]
+
+
+// <[fill
+const filled_nr_array = nr_array.fill(0, 2, 3)
+// [1, 2, 0, 0, 5]
+// (valueToFill, startIndex, endIndex)
+// [https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/fill]
+
+
+// <[flat
+const flattened_nr_array = [...nr_array, [6, 7]].flat()
+// [1, 2, 3, 4, 5, 6, 7]
+// Allows to specify the depth, in cases like [1, 2, [[[3], 4]], 5]
+// [https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat]
+
+
+// <[flatMap
+const flattened_mapped_nr_array = [...nr_array, [6, 7]].flatMap(el => el + 1)
+// [2, 3, 4, 5, 6, 7, 8]
+// can only flatten by one level
+// [https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flatMap]
+
+
+// 
 
