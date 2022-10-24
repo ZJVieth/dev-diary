b6fd9ed778ef9b228873e3e47746f2ecfe6c9c79
Zino Vieth <52859000+ZJVieth@users.noreply.github.com>
2022-10-25 03:17:54 +0900

    Create matterjs.js

---
+++ webdev/jslibs/matterjs.js
@@ -0,0 +1,13 @@
+/**
+ * # Matter.js: Javascript Physics Engine
+ * <[js <[physics <[simulation <[game
+ *
+ * <°A javascript library to simulate physica and rigid bodies on a canvas.°>
+ *
+ * Install:
+ * $ npm install matter-js
+ *
+ * Source Include:
+ * <script src="https://cdnjs.cloudflare.com/ajax/libs/matter-js/0.18.0/matter.min.js"></script>
+ *
+ */
